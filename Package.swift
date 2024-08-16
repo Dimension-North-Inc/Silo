d6abc7f2432179dbec8a5458a5e254f62e50d454
Mark Onyschuk <mark.onyschuk@ndimensionl.com>
2024-08-16 12:27:31 -0500

    Remove swift-testing, expect dependencies

--- Package.swift
+++ Package.swift
@@ -18,10 +18,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/pointfreeco/swift-clocks.git", from: "1.0.2"),
-        .package(url: "https://github.com/pointfreeco/swift-case-paths.git", branch: "main"),
+        .package(url: "https://github.com/pointfreeco/swift-case-paths.git", from: "1.1.0"),
         .package(url: "https://github.com/pointfreeco/swift-identified-collections.git", from: "1.0.0"),
-        
-        .package(url: "git@github.com:Dimension-North-Inc/swift-testing.git", from: "0.10.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -36,6 +34,6 @@ let package = Package(
         ),
         .testTarget(
             name: "SiloTests",
-            dependencies: ["Silo", "swift-testing"]),
+            dependencies: ["Silo"])
     ]
 )
